f9fc401426709c1272fdaf5783ac755300acea0c
Devis Battisti <devis.battisti@uqido.com>
2020-11-25 15:54:51 +0100

    db cfg

--- config/database.js
+++ config/database.js
@@ -12,9 +12,9 @@ module.exports = ({ env }) => ({
 //        password: env('DATABASE_PASSWORD', 'postgres'),
         host: 'ec2-3-220-98-137.compute-1.amazonaws.com',
         port: 5432,
-        database: d8tv2h39hk7gm2,
-        username: wlgzxedbfgyaaa,
-        password: a0bbf383621ad78a7b5a3cae5a46fb353ced097442354fd60f694664579c3ec,
+        database: 'd8tv2h39hk7gm2',
+        username: 'wlgzxedbfgyaaa',
+        password: 'a0bbf383621ad78a7b5a3cae5a46fb353ced097442354fd60f694664579c3ec',
         ssl: { "rejectUnauthorized": false }
         //ssl: env.bool('DATABASE_SSL', false),
       },
