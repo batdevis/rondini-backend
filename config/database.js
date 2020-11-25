7f2211e91d65b4a89b56e707bd80d2ad4fe16689
Devis Battisti <devis.battisti@uqido.com>
2020-11-25 16:19:38 +0100

    db port

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
 //        username: env('DATABASE_USERNAME', 'postgres'),
 //        password: env('DATABASE_PASSWORD', 'postgres'),
         host: 'ec2-3-220-98-137.compute-1.amazonaws.com',
-        port: 5432,
+        port: '5432',
         database: 'd8tv2h39hk7gm2',
         username: 'wlgzxedbfgyaaa',
         password: 'a0bbf383621ad78a7b5a3cae5a46fb353ced097442354fd60f694664579c3ec',
